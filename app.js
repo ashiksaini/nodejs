8235b5e799d04b1664bd3c8bae93403edd64e49e
ashiksaini <ashik.saini@aurigait.com>
2024-01-07 21:00:38 +0530

    simple express js setup

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-import http from 'http';
+// import http from core node
+import http from "http";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+// import express from express package
+import express from "express";
+
+// This will give as an express app for handling the all the features
+// that express ships.
+const expressApp = express();
+
+// Express way to handle routes
+// This "/" will handle all the req that comes in as it does not define any specific path.
+expressApp.use("/", (req, res, next) => {
+  console.log("/ Routes");
 });
 
-server.listen(8000);
+// Add middleware to express app
+expressApp.use((req, res, next) => {
+  console.log("In middleware 1");
 
+  // This allows request to continue to next middleware
+  // here i.e., middleware2
+  next();
+});
 
+// Another middleware
+expressApp.use((req, res, next) => {
+  console.log("In middleware 2");
+
+  // We can send response as
+  res.send("<h1> Hello from express js </h1>");
+});
+
+// Express way to create and listen server
+expressApp.listen(8000);
+
+// Node JS way to create and listen server
+// Create a server
+/*
+    const server = http.createServer(expressApp);
+    server.listen(8000);
+  */
